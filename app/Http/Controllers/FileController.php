5cf8afaeeb68496faf0bbd252574b157a0640912
Nagalakshmi P <nisj@equadriga.com>
2020-10-20 15:48:51 +0530

    models and service updated

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -1,17 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use App\Http\Services\FileStatusService;
+use App\Http\Models\FileMaster;
+use App\User;
+
 use Illuminate\Http\Request;
 
 class FileController extends Controller
 {
     public function Create()
     {
-        return view('create');
+        $officer = User:: orderBy('id')->get();
+        return view('create',compact('officer'));
     }
-
     public function retrieveFileStatusMaster() {
         $statuses = (new FileStatusService())->retrieveFileStatuses();
     }
@@ -22,4 +24,28 @@ class FileController extends Controller
             'fileList'
         ));
     }
+
+    public function fileCreate(Request $request) {
+        $file_data = $request->all();
+        $officer = User:: orderBy('id')->get();
+        $file_upload = "" ;
+       $filesupload_data = (new FileStatusService())->saveFileData($file_data,$file_upload);       
+        
+        $fileList = (new FileStatusService())->showFileStatus($filesupload_data);
+        return view('filestatuslist', compact(
+            'fileList','officer'
+        ));
+
+        //  print_r($filesupload_data->getContent());
+  
+    }
+    public function filedataupdate(Request $request){
+        $officer = User:: orderBy('id')->get();
+        $statuses = (new FileStatusService())->retrieveFileStatuses();
+        $fileupdated_data = (new FileStatusService())->fileUpdate($request->id);
+        
+        return view('fileupdate',compact('fileupdated_data','officer','statuses'));
+
+    }
 }
+
--- app/Http/Model/FileStatus.php
+++ app/Http/Model/FileStatus.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Model;
+namespace App\Http\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class FileStatus extends Model
 {
-    protected $table = 'file_status';
+    protected $table = 'file_statuses';
 }
