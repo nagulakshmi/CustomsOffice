a5ca97eb0425f185d0a2c711e50a737d979d1974
Nagalakshmi P <nisj@equadriga.com>
2020-10-20 17:06:42 +0530

    web routes added

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,22 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'DashboardController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/home','DashboardController@dashboard')->name('dashboard');
+Route::get('/home','HomeController@dashboard')->name('dashboard');
 
-Route::get('/create','FileController@create')->name('createnew');
+Route::get('/create','FileController@create')->name('create');
+
+Route::post('/savefile','FileController@fileCreate')->name('createnew');
 
 Route::get('/filestatuslist', 'FileController@showFileStatus')->name('latestfilelist');
 
+Route::match(['get', 'post'], '/fileslist', 'FileController@showFileStatus')->name('filelist');
+
 Route::get('/filestatuses', 'FileController@retrieveFileStatusMaster')->name('file_statuses');
+
+Route::match(['get','post'], '/filedata/edit/{id}','FileController@filedataupdate')->name('file_update');
+
+Route::get('/filedata/delete', 'FileController@deleteFile')->name('deletefile');
+
+
